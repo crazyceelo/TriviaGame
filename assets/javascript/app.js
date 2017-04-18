12c3e5c96ac305c87e2c87d495c8dcff37cd0954
Carlos Perez <solracias@gmail.com>
2017-04-17 22:18:12 -0700

    only added 2 looping questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,7 +38,7 @@ $(document).ready(function()
 		"answer4" : "answer 4"
 	};
 
-var q2 = 
+	var q2 = 
 	{
 		"question" : "question 2", 
 		"answer1" : "answer 1", 
@@ -47,7 +47,7 @@ var q2 =
 		"answer4" : "answer 4"
 	};
 
-var q3 = 
+	var q3 = 
 	{
 		"question" : "question 3", 
 		"answer1" : "answer 1", 
@@ -56,7 +56,7 @@ var q3 =
 		"answer4" : "answer 4"
 	};
 
-var q4 = 
+	var q4 = 
 	{
 		"question" : "question 4", 
 		"answer1" : "answer 1", 
@@ -65,23 +65,23 @@ var q4 =
 		"answer4" : "answer 4"
 	};
 
-var question = $("#question");
-var opt1 = $("#opt1");
-var opt2 = $("#opt2");
-var opt3 = $("#opt3");
-var opt4 = $("#opt4");
+	var question = $("#question");
+	var opt1 = $("#opt1");
+	var opt2 = $("#opt2");
+	var opt3 = $("#opt3");
+	var opt4 = $("#opt4");
 
-var correctAnswers = 0;
-var incorrectAnswers = 0;
+	var correctAnswers = 0;
+	var incorrectAnswers = 0;
 
-var interval1;
-var interval2;
+	var interval1;
+	var interval2;
 
-var timer = 
-{
-	"question" : 30,
-	"answer" : 6,
-}
+	var timer = 
+	{
+		"question" : 30,
+		"answer" : 6,
+	}
 
 	function startGame(q)
 	{
@@ -115,6 +115,8 @@ var timer =
 			clearInterval(interval2);
 			timer.question = 31;
 			questionCountdown();
+			newQuestion(q2);
+			runGame();
 		}
 	}
 
@@ -129,13 +131,33 @@ var timer =
 
 			// replace question div with out of time message
 			$("#question").before("<h4 id='correction'>test</h4>");
-			$("#question").replaceWith('<img src="http://i938.photobucket.com/albums/ad230/JoeTEStrikesBack/th_TimeUp.gif" alt="Wrong Answer" height="200" width="200">');
+			$("#question").replaceWith('<img id="image" src="http://i938.photobucket.com/albums/ad230/JoeTEStrikesBack/th_TimeUp.gif" alt="Wrong Answer" height="200" width="200">');
 
 			clearInterval(interval1);
 			timer.answer = 6;
 			// stop counting down.
 			answerCountDown();
 		}
+	}
+
+	function newQuestion(x) 
+	{
+		$("#correction").remove();
+		$("#image").replaceWith('<h3 id="question">question test</h3>');
+		$(".answer1-col").append('<h2 id="opt1" class="answer">opt1</h2>');
+		$(".answer2-col").append('<h2 id="opt2" class="answer">opt1</h2>');
+		$(".answer3-col").append('<h2 id="opt3" class="answer">opt1</h2>');
+		$(".answer4-col").append('<h2 id="opt4" class="answer">opt1</h2>');
+		$("#question").text(x.question);
+		$("#opt1").text(x.answer1);
+		$("#opt2").text(x.answer2);
+		$("#opt3").text(x.answer3);
+		$("#opt4").text(x.answer4);
+		$("#opt1").attr("data-value", "correct");
+		$("#opt2").attr("data-value", "incorrect");
+		$("#opt3").attr("data-value", "incorrect");
+		$("#opt4").attr("data-value", "incorrect");
+
 	}
 
 	function valueAssignment(q)
@@ -161,7 +183,6 @@ var timer =
 			}
 		})
 	}
-	
 
 	startGame(q1);
 	runGame();
