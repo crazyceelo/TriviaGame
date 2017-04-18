d5f194ec9de7cb1bfb1eadea3f3498329f714b9e
Carlos Perez <solracias@gmail.com>
2017-04-17 17:28:35 -0700

    time remaing loop is done.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,18 +31,12 @@ $(document).ready(function()
 {
 	var q1 = 
 	{
-		"question" : "question 1", 
+		"question" : "This is question 1", 
 		"answer1" : "answer 1", 
 		"answer2" : "answer 2",
 		"answer3" : "answer 3",
 		"answer4" : "answer 4"
 	};
-
-	console.log(q1.question);
-	console.log(q1.answer1);
-	console.log(q1.answer2);
-	console.log(q1.answer3);
-	console.log(q1.answer4);
 
 var q2 = 
 	{
@@ -71,25 +65,105 @@ var q4 =
 		"answer4" : "answer 4"
 	};
 
-var questionCounter = 30;
-var answerCounter = 5;
+var question = $("#question");
+var opt1 = $("#opt1");
+var opt2 = $("#opt2");
+var opt3 = $("#opt3");
+var opt4 = $("#opt4");
 
-	function startGame()
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
+var interval1;
+var interval2;
+
+var timer = 
+{
+	"question" : 30,
+	"answer" : 6,
+}
+
+	function startGame(q)
 	{
-		$("#question").text(q1.question);
-		$("#opt1").text(q1.answer1);
-		$("#opt2").text(q1.answer2);
-		$("#opt3").text(q1.answer3);
-		$("#opt4").text(q1.answer4);
-		setTimeout($("#seconds").text(questionCounter);
-		
+		question.text(q.question);
+		opt1.text(q.answer1);
+		opt2.text(q.answer2);
+		opt3.text(q.answer3);
+		opt4.text(q.answer4);
+		valueAssignment(q);
+		$("#seconds").html(timer.question);
+		questionCountdown();
 	}
 
-	function runGame(){
+	function questionCountdown()
+	{
+		interval1 = setInterval(questionDecrement, 1000);
+	}
+
+	function answerCountDown()
+	{
+		interval2 = setInterval(answerDecrement, 1000);
 
 	}
 
-	startGame();
+	function answerDecrement()
+	{
+		timer.answer--;
+		$("#seconds").html(timer.answer);
+		if (timer.answer == 0) 
+		{
+			clearInterval(interval2);
+			timer.question = 31;
+			questionCountdown();
+		}
+	}
+
+	function questionDecrement() 
+	{
+		timer.question--;
+		$("#seconds").html(timer.question);
+		if (timer.question == 0) 
+		{
+			// remove opt1, opt2, opt3, opt4
+			$(".answer").remove();
+
+			// replace question div with out of time message
+			$("#question").before("<h4 id='correction'>test</h4>");
+			$("#question").replaceWith('<img src="http://i938.photobucket.com/albums/ad230/JoeTEStrikesBack/th_TimeUp.gif" alt="Wrong Answer" height="200" width="200">');
+
+			clearInterval(interval1);
+			// stop counting down.
+			answerCountDown();
+		}
+	}
+
+	function valueAssignment(q)
+	{
+		opt1.attr("data-value", "correct");
+		opt2.attr("data-value", "incorrect");
+		opt3.attr("data-value", "incorrect");
+		opt4.attr("data-value", "incorrect");
+	}
+
+	function runGame()
+	{
+		$(".answer").on("click", function(){
+
+			if ($(this).attr("data-value") == "correct") 
+			{
+				alert("correct");
+			}
+
+			else if ($(this).attr("data-value") == "incorrect")
+			{
+				alert("incorrect");
+			}
+		})
+	}
+	
+
+	startGame(q1);
+	runGame();
+	
 })
 
-//objects
