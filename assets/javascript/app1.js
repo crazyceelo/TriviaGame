a179ea599ce9fed078e35b134b0c5a18b1d5fb49
caperez <caperez@blizzard.com>
2017-04-20 15:59:04 -0700

    added incorrect logic, but not complete yet

--- assets/javascript/app1.js
+++ assets/javascript/app1.js
@@ -69,6 +69,7 @@ $(document).ready(function()
 			else if ($(this).attr("data-value") == "incorrect")
 			{
 				incorrectAnswers++;
+				countDown(incorrectDecrement);
 				// incorrectDecrement()
 			}
 		})
@@ -99,6 +100,29 @@ $(document).ready(function()
 		// replace question div with out of time message
 		$("#question").before("<h4 id='correction'>correct answer is: "+ q1.answer1 +"</h4>");
 		$("#question").replaceWith('<img id="image" src="http://i938.photobucket.com/albums/ad230/JoeTEStrikesBack/th_TimeUp.gif" alt="Wrong Answer" height="200" width="200">');
+		timer.answer--;
+		$("#seconds").html(timer.answer);
+		if (timer.answer === 0) 
+		{
+			clearInterval(interval);
+			timer.answer = 6;
+			$("#correction").remove();
+			$("#image").replaceWith('<h3 id="question"></h3>');
+			$(".answer1-col").append('<h2 id="opt1" class="answer"></h2>');
+			$(".answer2-col").append('<h2 id="opt2" class="answer"></h2>');
+			$(".answer3-col").append('<h2 id="opt3" class="answer"></h2>');
+			$(".answer4-col").append('<h2 id="opt4" class="answer"></h2>');
+		}
+	}
+
+	function incorrectDecrement()
+	{
+		// remove opt1, opt2, opt3, opt4
+		$(".answer").remove();
+
+		// replace question div with out of time message
+		$("#question").before("<h4 id='correction'>correct answer is: "+ q1.answer1 +"</h4>");
+		$("#question").replaceWith('<img id="image" src="https://media.giphy.com/media/4OJFCEeGzYGs0/giphy.gif">');
 		timer.answer--;
 		$("#seconds").html(timer.answer);
 		if (timer.answer === 0) 
